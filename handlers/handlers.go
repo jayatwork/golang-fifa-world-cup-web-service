b7ddb4234897658e4559ea568d1f7703e133bf5b
Jason Ancrum <jason.ancrum00@delta.com>
2022-01-31 17:02:31 -0500

    needed to add personal fork on imports

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"golang-fifa-world-cup-web-service/data"
 	"net/http"
 )
 
@@ -13,22 +14,21 @@ func RootHandler(res http.ResponseWriter, req *http.Request) {
 // ListWinners returns winners from the list
 func ListWinners(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "application/json")
-	year := req.URL.Query().Get("year")
 
-	if year == " " {
-		winners, err := data.ListAllJSON(year)
+	year := req.URL.Query().Get("year")
+	if year == "" {
+		winners, err := data.ListAllJSON()
 		if err != nil {
 			res.WriteHeader(http.StatusInternalServerError)
-			return
 		}
 		res.Write(winners)
 	} else {
 		filteredWinners, err := data.ListAllByYear(year)
 		if err != nil {
 			res.WriteHeader(http.StatusBadRequest)
-		} else {
-			res.Write(filteredWinners)
+			return
 		}
+		res.Write(filteredWinners)
 	}
 
 }
